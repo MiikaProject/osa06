df65ca06099a66f435fde099543105a242c970d6
Miika <miika.roiha@gmail.com>
2019-03-02 13:40:06 +0200

    ekatehtava

--- unicafe-redux/src/reducer.js
+++ unicafe-redux/src/reducer.js
@@ -1,4 +1,4 @@
-const initialState = {
+let initialState = {
   good: 0,
   ok: 0,
   bad: 0
@@ -8,16 +8,50 @@ const counterReducer = (state = initialState, action) => {
   console.log(action)
   switch (action.type) {
     case 'GOOD':
+
+      const newState = {
+        good: state.good+1,
+        ok: state.ok,
+        bad: state.bad
+      }
+      state = newState
+
       return state
     case 'OK':
+
+    const newState2 = {
+      good: state.good,
+      ok: state.ok+1,
+      bad: state.bad
+    }
+
+    state = newState2
       return state
+
     case 'BAD':
+
+    const newState3 = {
+      good: state.good,
+      ok: state.ok,
+      bad: state.bad+1
+    }
+    state = newState3
+    
       return state
     case 'ZERO':
+
+      const newState4 = {
+        good: 0,
+        ok: 0,
+        bad: 0
+      }
+
+      state = newState4
+
       return state
     default: return state
   }
-  
+
 }
 
 export default counterReducer
--- unicafe-redux/src/reducer.test.js
+++ unicafe-redux/src/reducer.test.js
@@ -32,4 +32,55 @@ describe('unicafe reducer', () => {
       bad: 0
     })
   })
+
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('zero works', () => {
+    const action = {
+      type: 'ZERO'
+    }
+
+    const alkutilanne = {
+      good : 2,
+      ok : 3,
+      bad : 4
+    }
+    const state = alkutilanne
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 0
+    })
+  })
 })
